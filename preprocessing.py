b91a247e1ecd5f2876afec005d16380e79238903
rahmadyan <rahmadyan98@gmail.com>
2019-05-07 02:24:48 +0700

    preprocessing v3

--- preprocessing.py
+++ preprocessing.py
@@ -15,8 +15,10 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 mycursor.execute("SELECT content FROM news_tb")
 documents = mycursor.fetchall()
+for x in documents:
+    print(x)
 
-#token stem stop
+#1. tokenizing stopword dan stemming
 dictOfWords = {}
 
 for index, sentence in enumerate(documents):
@@ -36,7 +38,7 @@ for index, sentence in enumerate(documents):
     dictOfWords[index] = [(word,wordsFiltered.count(word)) for word in wordsFiltered]
 print(dictOfWords)
 
-#Menghilangkan kata duplikat
+#2. Menghilangkan kata duplikat
 termFrequency = {}
 
 for i in range(0, len(documents)):
@@ -46,93 +48,43 @@ for i in range(0, len(documents)):
             listOfNoDuplicates.append(wordFreq)
         termFrequency[i] = listOfNoDuplicates
 print(termFrequency)
-#
-# #Third: normalized term frequency
-# normalizedTermFrequency = {}
-# for i in range(0, len(documents)):
-#     sentence = dictOfWords[i]
-#     lenOfSentence = len(sentence)
-#     listOfNormalized = []
-#     for wordFreq in termFrequency[i]:
-#         normalizedFreq = wordFreq[1]/lenOfSentence
-#         listOfNormalized.append((wordFreq[0],normalizedFreq))
-#     normalizedTermFrequency[i] = listOfNormalized
-#
-# #print(normalizedTermFrequency)
-#
-#
-# #---Calculate IDF
-#
-# #First: put al sentences together and tokenze words
-#
-# allDocuments = ''
-# for sentence in documents:
-#     allDocuments += sentence + ' '
+
+#3. Normalisasi TF
+#Kemunculan kata/istilah(t) dalam kalimat / jumlah kata dalam dokumen/kalimat(d)
+normalizedTermFrequency = {}
+for i in range(0, len(documents)):
+    sentence = dictOfWords[i]
+    lenOfSentence = len(sentence) #menghitung jumlah kata dalam kalimat
+    listOfNormalized = []
+    for wordFreq in termFrequency[i]:
+        normalizedFreq = wordFreq[1]/lenOfSentence #pembagain kemunculan kata dengan jumlah kata dalam kalimat
+        listOfNormalized.append((wordFreq[0],normalizedFreq))
+    normalizedTermFrequency[i] = listOfNormalized
+print(normalizedTermFrequency)
+
+#4.IDF ngelu
+# RUMUS = log(n/df) n= jumlah dokumen df = jumlah dimana istilah/kata muncul
+
+allDocuments = ''
+for sentence in documents:
+    for f in sentence:
+        allDocuments += f + ' '
+# print(allDocuments)
 # allDocumentsTokenized = allDocuments.split(' ')
-#
-# #print(allDocumentsTokenized)
+
+# print(allDocumentsTokenized)
 #
 # allDocumentsNoDuplicates = []
 #
 # for word in allDocumentsTokenized:
 #     if word not in allDocumentsNoDuplicates:
 #         allDocumentsNoDuplicates.append(word)
-#
-#
-# #print(allDocumentsNoDuplicates)
-#
-# #Second calculate the number of documents where the term t appears
-#
-# dictOfNumberOfDocumentsWithTermInside = {}
-#
-# for index, voc in enumerate(allDocumentsNoDuplicates):
-#     count = 0
-#     for sentence in documents:
-#         if voc in sentence:
-#             count += 1
-#     dictOfNumberOfDocumentsWithTermInside[index] = (voc, count)
-#
-# #print(dictOfNumberOfDocumentsWithTermInside)
-#
-#
-# #calculate IDF
-#
-# dictOFIDFNoDuplicates = {}
-#
-#
-# for i in range(0, len(normalizedTermFrequency)):
-#     listOfIDFCalcs = []
-#     for word in normalizedTermFrequency[i]:
-#         for x in range(0, len(dictOfNumberOfDocumentsWithTermInside)):
-#             if word[0] == dictOfNumberOfDocumentsWithTermInside[x][0]:
-#                 listOfIDFCalcs.append((word[0],math.log(len(documents)/dictOfNumberOfDocumentsWithTermInside[x][1])))
-#     dictOFIDFNoDuplicates[i] = listOfIDFCalcs
-#
-# #print(dictOFIDFNoDuplicates)
-#
-# #Multiply tf by idf for tf-idf
-#
-# dictOFTF_IDF = {}
-# for i in range(0,len(normalizedTermFrequency)):
-#     listOFTF_IDF = []
-#     TFsentence = normalizedTermFrequency[i]
-#     IDFsentence = dictOFIDFNoDuplicates[i]
-#     for x in range(0, len(TFsentence)):
-#         listOFTF_IDF.append((TFsentence[x][0],TFsentence[x][1]*IDFsentence[x][1]))
-#     dictOFTF_IDF[i] = listOFTF_IDF
-#
-# print(dictOFTF_IDF)
-#
-# from nltk.text import TextCollection
-#
-# mytexts = TextCollection(['the the universe has very many stars','the galaxy contains many stars','the cold breeze of winter made it very cold outside'])
-#
-# print("NLTK tf_idf")
-# print(mytexts.tf_idf('very','the the universe has very many stars'))
-#
-# def test_tf(term, text):
-#     newText = text.split(' ')
-#     print(text.count(term))
-#     print(len(newText))
+# allDocumentsNoDuplicates = []
 
-#test_tf('universe','the the universe has very many stars')
+#IKI PERCOBAAN JANGAN HIRAUKAN
+# for t in tokenizedWords:
+#     if t not in listStopword:
+#         allDocumentsNoDuplicates.append(t)
+# for w in allDocumentsNoDuplicates:
+#     print(w)
+

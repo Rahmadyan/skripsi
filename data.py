dfb4e30aeeb1e9bd062112e60c586cb16dfafc85
rahmadyan <rahmadyan98@gmail.com>
2019-05-20 01:52:59 +0700

    baruu

---
+++ data.py
@@ -0,0 +1,28 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title': 'Article One',
+            'body':'auwhdiyagwiydhdwhiahwdhawudhawihd ihawih uiahwidh ihauihawhdu huiawhdih uah ',
+            'author':'oawkdo',
+            'creade_date':'09-89-0'
+
+        },
+        {
+            'id': 2,
+            'title': 'Article Twu',
+            'body': 'auwhdiyagwiydhdwhiahwdhawudhawihd ihawih uiahwidh ihauihawhdu huiawhdih uah ',
+            'author': 'oawkdo',
+            'creade_date': '09-89-0'
+
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'auwhdiyagwiydhdwhiahwdhawudhawihd ihawih uiahwidh ihauihawhdu huiawhdih uah ',
+            'author': 'oawkdo',
+            'creade_date': '09-89-0'
+
+        }
+    ]
+    return articles
